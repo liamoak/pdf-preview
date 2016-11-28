9da1d2e74d1cadf78aebbae9311ad23cf66ae02a
Liam McNally <liam@oaksi.com>
2016-11-27 20:27:15 -0500

    pages show in boxes

--- mypdf.js
+++ mypdf.js
@@ -1,26 +1,48 @@
-PDFJS.workerSrc = "pdf.js";
+PDFJS.workerSrc = "pdf.js"; // location of pdf.js file
 
 PDFJS.getDocument('lm741.pdf').then(function (pdf) {
     // you can now use *pdf* here
-    console.log(pdf.pdfInfo.numPages); // thats the number of pages
-    pdf.getPage(1).then(function (page) {
-        // you can now use *page* here
-       // var scale = 1.5;
-       // var viewport = page.getViewport(scale);
-       var desiredWidth = 240;
-       var viewport = page.getViewport(1);
-       var scale = desiredWidth / viewport.width;
-       var viewport = page.getViewport(scale);
+    
+    // For each page in the pdf, push some information onto the pagesData array
+    var pagesData = [];
+    for (var i = 1; i <= pdf.pdfInfo.numPages; i++) {
+        pagesData.push(
+            { number: i.toString(), canvasid: "the-canvas".concat(i.toString()) }
+        );
+    }
+    // once all pages are added to the data array, render the html specified via a template in index.html where the #html-template is used.
+    var theTemplateScript = $("#page-template").html();
+    var theTemplate = Handlebars.compile(theTemplateScript);
+    $(".handleBars").append(theTemplate(pagesData));
 
-        var canvas = document.getElementById('the-canvas');
-        var context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        var renderContext = {
-            canvasContext: context,
-            viewport: viewport
-        };
-        page.render(renderContext);
-    });
+    // once the boxes for the pages ares setup and are denoted by id=canvasID. render the page in each box.
+    for (var i = 1; i <= pdf.pdfInfo.numPages; i++) {
+        let ID = 'the-canvas'.concat(i.toString());
+        console.log(ID);
+        pdf.getPage(i).then(function (page) { showPage(page, ID) });
+    }
 });
+
+// these canvas IDs must exist prior to running this loop.
+// This means that the handlebar boxes must be rendered prior to the content.
+
+function showPage(page, canvasID) {
+    // you can now use *page* here
+    // var scale = 1.5;
+    // var viewport = page.getViewport(scale);
+    var desiredWidth = 240;
+    var viewport = page.getViewport(1);
+    var scale = desiredWidth / viewport.width;
+    var viewport = page.getViewport(scale);
+
+    var canvas = document.getElementById(canvasID);
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+    };
+    page.render(renderContext);
+}
